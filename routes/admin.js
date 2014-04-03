7f781f8f941c34e789a2b996a40a2cf92c641015
Thibaut Derodit <thibaut.derodit@gmail.com>
2014-04-03 17:36:10 +0200

    Ajout : Ajout d'un professeur et d'un étudiant dans la console d'administration

--- routes/admin.js
+++ routes/admin.js
@@ -28,7 +28,6 @@ exports.loginAdminPost = function(req, res) {
 	}
 
 	if(!errors.length) {
-		console.log(adminPost);
 		var admin = require("../models/admin");
 		admin.isAdminCorrect(adminPost.name, adminPost.passwd, function(err, result){
 			if(err) {
@@ -71,7 +70,62 @@ exports.addPersonAdmin = function(req, res) {
  * Vérifie et ajoute un professeur ou un étudiant dans la base
  */
 exports.addPersonAdminPost = function(req, res) {
-	res.render('admin/addPerson', {
-		title: 'Ajout'
-	});
+	var errors = [];
+	if(req.body.teacher) {
+		var teacherPost = req.body.teacher;
+		if(teacherPost.name === "") {
+			errors.push("Veuiller entrer un nom");
+		}
+		if(teacherPost.passwd === "") {
+			errors.push("Veuiller entrer un mot de passe");
+		}
+
+		if(!errors.length) {
+			var teacher = require('../models/teacher');
+			teacher.addTeacher(teacherPost.name, teacherPost.passwd, function(inserted){
+				res.render('admin/addPerson', {
+					title: 'Ajout',
+					success: {
+						name : teacherPost.name,
+						type: "teacher"
+					}
+				});	
+			});
+		}
+		else {
+			res.render('admin/addPerson', {
+				title: 'Ajout',
+				errors: errors
+			});
+		}
+
+	}
+	else if(req.body.student) {
+		var studentPost = req.body.student;
+		if(studentPost.name === "") {
+			errors.push("Veuiller entrer un nom");
+		}
+		if(studentPost.email === "") {
+			errors.push("Veuiller entrer une adresse mail");
+		}
+
+		if(!errors.length) {
+			var student = require('../models/student');
+			student.addStudent(studentPost.name, studentPost.email, function(inserted){
+				res.render('admin/addPerson', {
+					title: 'Ajout',
+					success: {
+						name : studentPost.name,
+						type: "student"
+					}
+				});	
+			});
+		}
+		else {
+			res.render('admin/addPerson', {
+				title: 'Ajout',
+				errors: errors
+			});
+		}
+	}
 }
